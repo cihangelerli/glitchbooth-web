64d4008adddae9776db87e423f4636b49fabe27c
gelerli <82060355+gelerli@users.noreply.github.com>
2026-06-15 12:57:59 +0300

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react"; // 1. Change /next to /react
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics /> {/* 2. Mount the component inside your global root wrapper */}
   </StrictMode>,
 );
